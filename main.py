26965411052ddd5f6cf3c48456b2326d088d4197
Nikos Kanakaris <nikos.kanakaris89@gmail.com>
2021-03-01 20:45:49 +0200

    Integrate docker

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import uvicorn
+from decouple import config
 
 if __name__ == '__main__':
-    uvicorn.run("server.app:app", host='0.0.0.0', port=8000, reload=True)
+    reload = config('MAIN_BACKEND_RELOAD', cast=bool)
+    number_of_workers = config('MAIN_BACKEND_NUMBER_OF_WORKERS', cast=int)
+    uvicorn.run("server.app:app", host='0.0.0.0', port=8000, reload=reload, workers=number_of_workers)
